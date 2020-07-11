746de4ec48210abdfc3d8dd3cd23c49b78890d7c
abraham sanchez fonseca <abraham_gto@hotmail.com>
2020-07-10 20:09:07 -0500

    Show Read Options only when a book is loaded

--- src/app/component/epub/text-to-speach/epub-reader-options/epub-reader-options.component.ts
+++ src/app/component/epub/text-to-speach/epub-reader-options/epub-reader-options.component.ts
@@ -9,9 +9,13 @@ import { EpubService } from 'src/app/service/epub/epub.service';
 export class EpubReaderOptionsComponent implements OnInit {
   readingAtm: boolean;
 
+  hasBook: boolean;
   constructor(private epubService: EpubService) {
     epubService.OnRead.subscribe((read) => {
       this.onRead(read);
+    });
+    epubService.onOpenEpub.subscribe((book) => {
+      this.hasBook = book != null;
     });
   }
 
@@ -23,24 +27,34 @@ export class EpubReaderOptionsComponent implements OnInit {
     return this.readingAtm;
   }
 
+  //#region HTML Calls
+  //Toggle the reading of the text
   toggleRead(): void {
     this.readingAtm = !this.readingAtm;
     this.epubService.OnRead.emit(this.readingAtm);
   }
-
+  //Returns true if the loaded book is not null
+  showOptions(): boolean {
+    return this.hasBook;
+  }
+  //Read Next Paragraph
   readNext(): void {
     this.epubService.OnReadNext.emit(true);
   }
+  //Read Previews Paragraph
   readPreviews(): void {
     this.epubService.OnReadNext.emit(false);
   }
+  //Toggles the chapters menu
   showChapters(): void {
     this.epubService.toggleChapters();
   }
+  //Returns the correct text to be display int he read button
   getReadText(): string {
     if (this.readingAtm) {
       return 'Stop Read aloud';
     }
     return 'Read aloud';
   }
+  //#endregion
 }
