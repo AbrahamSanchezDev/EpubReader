df8479afc192f3bdaa6136374ee9d20a09d5d30b
abraham sanchez fonseca <abraham_gto@hotmail.com>
2020-07-11 20:28:31 -0500

    Code coverage on save data in local storage

--- src/app/service/save-to-local-storage/save-data-in-local-storage.service.spec.ts
+++ src/app/service/save-to-local-storage/save-data-in-local-storage.service.spec.ts
@@ -9,9 +9,10 @@ export class testingSave {
 export class testingSaveService extends SaveDataInLocalStorageService<
   testingSave
 > {}
-fdescribe('SaveDataInLocalStorageService', () => {
+describe('SaveDataInLocalStorageService', () => {
   let service: testingSaveService;
 
+  const testingData = new testingSave();
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = new testingSaveService();
@@ -19,5 +20,31 @@ fdescribe('SaveDataInLocalStorageService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should save the data', () => {
+    spyOn(localStorage, 'setItem');
+    service.saveDataFor(testingData.name, testingData);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('should load the data', () => {
+    spyOn(localStorage, 'getItem').and.callFake((key) => {
+      if (key == testingData.name) {
+        return JSON.stringify(testingData);
+      } else return null;
+    });
+    let loaded = service.loadDataFor(testingData.name);
+    expect(localStorage.getItem).toHaveBeenCalled();
+    expect(loaded.name).toBe(testingData.name);
+    //Check for some other key that is not saved
+    loaded = service.loadDataFor('some Other');
+    expect(loaded).toBe(null);
+  });
+
+  it('should delete the data', () => {
+    spyOn(localStorage, 'removeItem');
+    service.deleteDataFor(testingData.name);
+    expect(localStorage.removeItem).toHaveBeenCalled();
   });
 });
--- src/app/service/save-to-local-storage/save-data-in-local-storage.service.ts
+++ src/app/service/save-to-local-storage/save-data-in-local-storage.service.ts
@@ -9,10 +9,17 @@ export class SaveDataInLocalStorageService<T> {
   //Load the book data if there was any
   loadDataFor(fileName: string): T {
     const dataObj = localStorage.getItem(fileName);
+    if (dataObj == null) {
+      return null;
+    }
     return JSON.parse(dataObj) as T;
   }
   //Save the file data to the browser cache
   saveDataFor(fileName: string, object: T): void {
     localStorage.setItem(fileName, JSON.stringify(object));
+  }
+  //Save the file data to the browser cache
+  deleteDataFor(fileName: string): void {
+    localStorage.removeItem(fileName);
   }
 }
